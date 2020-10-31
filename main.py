412d4b65550e1d611663e58d4fb589eecf0c9076
Philipp Mulfinger <info@phil-splash.de>
2020-10-31 20:50:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+mood = input("How do you feel today?")
+
+if mood == "good":
+    print("Lucky guy! And you also do look fantastic!")
+elif mood == "sad":
+    print("Keep your head up, nobody is happy about the whole corona thing.")
+elif mood == "depressed":
+    print("Get help here: telephone counseling -GOOD MOOD-, 0172/29852 ")
+elif mood == "nervous":
+    print("Smoke some pot to calm down, please.")
+else:
+    print("Don't you have any feelings about this whole python thing?")
